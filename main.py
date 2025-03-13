3b41b5ff9b7e0c99c33fc997336f412decb01b51
liep22 <157056339+liep22@users.noreply.github.com>
2025-03-13 19:23:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import flask
 import pandas as pd
 import matplotlib.pyplot as plt
+import tkinter as tk
+from flask import Flask, render_template, url_for
 
 app = flask.Flask(__name__)
 
+
 @app.route('/')
-def sākums():
+def home():
+    return flask.render_template('home.html')
+
+@app.route('/stabini',methods=['GET'] )
+def stabini():
         
         df = pd.read_csv('projekts.2_prog.csv', delimiter=';')# Nolasiet CSV failu, norādot atdalītāju
-        
-        
         df.columns = df.columns.str.strip()# Noņemiet atstarpes no kolonnu nosaukumiem
         df = df.dropna()
         filtered_df = df[df['Dienas veids'].str.contains('Saulains|Apmācies ar sauli', na=False)]
@@ -24,7 +29,20 @@ def sākums():
         plt.title('Temperatūra')
         plt.subplots_adjust(bottom=0.3)  # Pielāgo figūras marginus
         plt.savefig('static/diagramma.png')  # Saglabā attēlu failā
-        return flask.render_template('index.html')
-    
+        return flask.render_template('stabini.html')
 
-app.run(debug=True)
+
+
+@app.route('/histogramma')
+def histogramma():
+    df = pd.read_csv('projekts.2_prog.csv', delimiter=';')
+    df.columns = df.columns.str.strip()  # Noņemiet atstarpes no kolonnu nosaukumiem
+    df = df.dropna()
+    filtrs_df = df[df['Dienas veids'].str.contains('Apmācies', na=False)]
+    filtrs_df.hist(column='Gaisa spiediens mmHg', bins = 10)
+    ax = filtrs_df.hist(column='Gaisa spiediens mmHg', bins=15)
+    plt.savefig('static/histogramma.png')
+    return flask.render_template('hist.html')
+    
+if __name__ == "__main__":
+ app.run(debug=True)
